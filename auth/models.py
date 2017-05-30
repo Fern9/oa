68cd82dd40e04493e90854d94c2084b4831e6d9f
Fern9 <mzfenng@gmail.com>
2017-05-31 01:51:52 +0800

    add role model and define simple permission. init three roles into mongodb

--- auth/models.py
+++ auth/models.py
@@ -1,7 +1,44 @@
 from oa import db
 import datetime
+from enum import Enum, unique
 
 
 class User(db.Document):
-    title = db.StringField()
-    create_time = db.DateTimeField(default=datetime.datetime.now)
+    wx_user = db.DictField()  # 用户微信信息
+    name = db.StringField(required=True)  # 姓名
+    address = db.StringField()  # 地址
+    company = db.StringField()  # 工作单位
+    age = db.IntField()  # 年龄
+    email = db.EmailField()  # 邮箱
+    register_time = db.DateTimeField(default=datetime.datetime.now)  # 注册时间
+    edit_time = db.DateTimeField(default=datetime.datetime.now)  # 最后编辑时间
+    role = db.ReferenceField('Role')
+
+
+class Role(db.Document):
+    name = db.StringField(unique=True)  # 角色名,唯一
+    permission = db.ListField()  # 权限列表
+    default = db.BooleanField()  # 是否为默认角色,默认角色为普通用户
+
+
+@unique
+class Permission(Enum):
+    APPLY_REQUIRE = 1  # 申请维修
+    EDIT_REPIRE_FORM = 2  # 编辑维修单
+    MANAGE_USER = 3  # 用户管理
+    AUTH_ROLE = 4  # 更改用户角色
+
+
+@staticmethod
+def init_roles():
+    for role in Role.objects.all():
+        role.delete()
+    common = Role(name="普通用户", permission=[Permission.APPLY_REQUIRE.value], default=True)
+    repairer = Role(name="维修人员", permission=[Permission.APPLY_REQUIRE.value, Permission.EDIT_REPIRE_FORM.value],
+                    default=False)
+    admin = Role(name="管理员", permission=[Permission.APPLY_REQUIRE.value, Permission.EDIT_REPIRE_FORM.value,
+                                         Permission.AUTH_ROLE.value,
+                                         Permission.MANAGE_USER.value], default=False)
+    common.save()
+    repairer.save()
+    admin.save()
--- auth/views.py
+++ auth/views.py
@@ -1,14 +1,27 @@
 import flask
-from .models import User
+from mongoengine import ValidationError
+
+from .models import User, Role
 from . import auth
 
 
 @auth.route('/user', methods=['PUT'])
 def register():
-    user = User(title='test_title')
-    user.save()
+    role = Role.objects(default=True).first()
+    print(role)
+    wx_user = {'id': '01', 'name': '陈宇航'}
+    user = User(wx_user=wx_user, name='陈酱', role=role)
+    try:
+        user.save()
+    except ValidationError as e:
+        return e.message
+
     return 'success'
+
 
 @auth.route('/test')
 def test():
+    print(User.objects(name='陈酱').first().role.name)
     return 'hello world'
+
+
