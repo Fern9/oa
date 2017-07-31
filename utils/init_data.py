01079126d1da810784ca173a10b6898a19f940fd
Fern9 <fern9@wemart.cn>
2017-07-31 21:44:02 +0800

    no message

--- utils/init_data.py
+++ utils/init_data.py
@@ -1,12 +1,15 @@
-from models import Role
 from process.procedure.process_procedure import Process
 from utils.data_helper import DataHelper
 from oa import app
+from models import *
 
 
 if __name__ == '__main__':
     try:
         app.app_context().push()
+        ProcessDefine.objects().delete()
+        ProcessInst.objects().delete()
+        ActivityInst.objects().delete()
         Process.init_process_define()
         roles = Role.objects().all()
         if roles is None:
